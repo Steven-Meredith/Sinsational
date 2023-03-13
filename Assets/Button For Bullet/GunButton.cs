dda0ecb36824e7feafbce7e199df5d60abfc2d4c
BurgerBobby <ethan.haynes@ontariotechu.net>
2023-03-12 22:39:36 -0400

    Button Update

--- Assets/Button For Bullet/GunButton.cs
+++ Assets/Button For Bullet/GunButton.cs
@@ -7,7 +7,7 @@ public class GunButton : MonoBehaviour
     //the light is basically a light for the switch, so you can toggle the switch on and off visually, and the object is the object that is being activated or deactivated ofc
     //[SerializeField] GameObject _lightObj;
     [SerializeField] List<GameObject> _object = new List<GameObject>();
-
+    [SerializeField] GameObject _lightObj;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -16,18 +16,18 @@ public class GunButton : MonoBehaviour
             for (int i = 0; i < _object.Count; i++)
             {
                 _object[i].SetActive(false);
+                _lightObj.SetActive(false);
             }
             // if the object is hit with a bullet
-            /*if (_object.activeSelf != true)
+            /*if (_lightObj.activeSelf != true)
             {
                 _lightObj.SetActive(true);
-                _object.SetActive(true);
+                
             }
             else
             {
                 _lightObj.SetActive(false);
-                _object.SetActive(false);
-            }*/
+            } */
         }
     }
 }
